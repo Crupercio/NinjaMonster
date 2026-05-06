39a1b6dbcc893af832c058f916be56896cec7aad
Luis Cruz Sanchez <luis.f.cruzsanchez@outlook.com>
2026-05-05 18:05:00 -0700

    Hardcode port 8000, remove PORT variable dependency

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 python manage.py migrate --noinput
 python manage.py createsuperuser --noinput || true
-exec daphne -b 0.0.0.0 -p "${PORT:-8000}" config.asgi:application
+exec daphne -b 0.0.0.0 -p 8000 config.asgi:application
